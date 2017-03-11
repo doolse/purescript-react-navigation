28a3e492f920227a5f9eedfc8119df69d86685af
Jens Krause <email@jkrause.io>
2017-03-11 18:15:46 +0100

    Update `HelloMobileExample` to add `Home` screen

--- examples/HelloMobileNavigation/src/Main.Ios.purs
+++ examples/HelloMobileNavigation/src/Main.Ios.purs
@@ -2,28 +2,18 @@ module Main.Ios where
 
 import Prelude
 import Control.Monad.Eff (Eff)
-import Data.Function.Uncurried (Fn0, mkFn0)
-import React (ReactClass, ReactElement, createClass, spec)
+import React (ReactClass, createClass, spec)
 import ReactNative.API (REGISTER, registerComponent)
 import ReactNative.Components.Text (text_)
 import ReactNative.Components.View (view_)
-import ReactNavigation (stackNavigator)
+import ReactNavigation (stackNavigator', applyNavigationOptions)
 
-homeView :: ReactElement
-homeView = view_ [ text_ "Hello, Navigation!" ]
-
--- Compiled PS code provides an object representing the module.
--- But any view used as screen has to be wrapped within a function to pass `validateRouteConfigMap`
--- See https://github.com/react-community/react-navigation/blob/master/src/routers/validateRouteConfigMap.js#L37
--- TODO: If there is no other way move it into ReactNavigation.purs
-mkScreen :: ReactElement -> Fn0 ReactElement
-mkScreen view = mkFn0 \_ -> view
+homeView :: ReactClass Unit
+homeView = createClass $ spec unit \_ ->
+  pure $ text_ "Hello, Navigation!"
 
 app :: ReactClass Unit
-app = createClass $ spec unit render
-  where
-    render ctx =
-      pure $ stackNavigator { home: { screen: mkScreen homeView } }
+app = stackNavigator' { home: { screen: applyNavigationOptions homeView {title: "Welcome"} } }
 
 main :: forall eff. Eff ( register :: REGISTER | eff) Unit
 main = registerComponent "HelloMobileNavigation" app
